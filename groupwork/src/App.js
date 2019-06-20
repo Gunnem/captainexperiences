85619ba55c722bfb6b78358ac118c12419908c53
Gunnem <sp3dng9@gmail.com>
2019-06-19 20:29:50 -0700

    Navbar and bootstrap added

--- groupwork/src/App.js
+++ groupwork/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Carousel from "./components/Carousel";
+import Nav from "./components/Nav";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <div className="image-carousel">
         <Carousel />
       </div>
